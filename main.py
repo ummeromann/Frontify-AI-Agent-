c03953c627b76f2ed7a0971c010c23b7110f8274
ummeromann <rummii.06@gmail.com>
2025-06-13 23:56:30 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,59 @@
+from agents import Runner, Agent, OpenAIChatCompletionsModel, AsyncOpenAI, RunConfig
+from openai.types.responses import ResponseTextDeltaEvent
+
+import os
+from dotenv import load_dotenv
+import chainlit as cl
+
+load_dotenv()
+
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+external_client = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/"
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client
+)
+
+config = RunConfig(
+    model=model,
+    model_provider=external_client,
+    tracing_disabled=True
+)
+
+agent = Agent(
+    name="Frontify AI",
+    instructions="You are a frontend expert",
+)
+
+@cl.on_chat_start
+async def handle_start():
+    cl.user_session.set("history", [])
+    await cl.Message(content="Hello from Umme Roman, How can i help you?").send()
+
+@cl.on_message
+async def handle_message(message : cl.Message):
+    
+    history = cl.user_session.get("history")
+    history.append({"role": "user", "content": message.content})
+
+    msg = cl.Message(content="")
+    await msg.send()
+                   
+    result = Runner.run_streamed(
+        agent,
+        input=history,
+        run_config=config
+    )
+
+    async for event in result.stream_events():
+        if event.type == "raw_response_event" and isinstance(event.data, ResponseTextDeltaEvent):
+            await msg.stream_token(event.data.delta)
+
+    history.append({"role": "assistant", "content": result.final_output})
+    cl.user_session.set("history", history)
+    # await cl.Message(content=result.final_output).send()
